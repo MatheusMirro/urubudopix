8af3bf1fb30d57be30fb7daf432bafe5c7427ea2
Matheus Alves <mirro123@outlook.com>
2024-10-14 03:04:03 -0300

    starter pack urubu do pix with a configuration to remove a login screen from spring security and design pattern TDD.

---
+++ src/main/java/com/mirro/urubudopix/UrubudopixApplication.java
@@ -0,0 +1,12 @@
+package com.mirro.urubudopix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrubudopixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrubudopixApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mirro/urubudopix/UrubudopixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirro.urubudopix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrubudopixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
